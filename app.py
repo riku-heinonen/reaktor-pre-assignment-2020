26155914a5835b9a8b8d0261c32d1ef93e9ec5d0
Riku Heinonen <riku.s.heinonen@aalto.fi>
2020-01-23 22:18:29 +0200

    Add basic parsing of package data
    The backend now parses the name, version and description of a package
    and returns the data as JSON on the homepage.

--- app.py
+++ app.py
@@ -6,11 +6,86 @@ import os
 
 # instantiate the app
 app = Flask(__name__)
+STATUS_FILE_PATH = os.path.abspath('/var/lib/dpkg/status') or 'static/status'
+
+
+class Package():
+    def __init__(self, name, version, description):
+        self.name = name
+        self.version = version
+        self.description = description
+
+    def serialize(self):
+        return {
+            'name': self.name,
+            'version': self.version,
+            'description': self.description
+        }
+
+    def __repr__(self):
+        return '<Package {}, version {}>'.format(self.name, self.version)
+
+
+def parse_package_data(package_string):
+    '''
+        Parses one entry of the status file (separated by a double newline). 
+        Gets the name, version and description of the package and returns
+        them as a Package object. 
+    '''
+    lines = package_string.splitlines(keepends=True)
+    line_index = 0
+    while line_index < len(lines):
+        line = lines[line_index]
+        if line.startswith('Package'):
+            name = line.split(': ', 1)[1].strip()
+
+        elif line.startswith('Version'):
+            version = line.split(': ', 1)[1].strip()
+
+        elif line.startswith('Description'):
+            description = line.split(': ', 1)[1]
+            # check for a multiline description, a multiline field is determined
+            # to begin every continuation line with a space ' ' or a tab '\t'
+            line_index += 1
+            next_line = lines[line_index]
+            while next_line[0] == ' ' or next_line[0] == '\t':
+                description += next_line
+                line_index += 1
+                if line_index >= len(lines):
+                    break
+                next_line = lines[line_index]
+
+            # get rid of the last newline
+            description = description.rstrip()
+
+        line_index += 1
+
+    return Package(name, version, description)
+
+
+def parse_packages(filename):
+    '''
+        Parses a dpkg status file and returns a dictionary of Package objects that contain
+        the name, version, description, dependencies and reverse dependencies of a package.
+    '''
+    packages = {}
+    with open(filename, 'r') as status_file:
+        raw_data = status_file.read()
+        # package entries are separated by a double new-line
+        data = raw_data.split('\n\n')
+        for package_string in data[:-2]:
+            package = parse_package_data(package_string)
+            packages[package.name] = package
+
+    return packages
+
+
+packages = parse_packages(STATUS_FILE_PATH)
 
 
 @app.route('/', methods=['GET'])
 def home():
-    return "Hello world"
+    return jsonify([package.serialize() for package in packages.values()])
 
 
 if __name__ == '__main__':
